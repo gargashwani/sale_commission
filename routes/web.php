6d5fcb6be69410b89d297bfe02186ca01917acca
Ashwani Garg <ashwanigarg@outlook.in>
2024-04-16 06:45:34 +0530

    merge

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\SaletypeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\SaletypeController;
 
 /*
 |--------------------------------------------------------------------------
